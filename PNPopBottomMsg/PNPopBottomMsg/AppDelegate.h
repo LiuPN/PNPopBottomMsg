b346449cf128f39088cab9a2762440648903a81a
liupanniu <panliuliu@139.com>
2015-09-08 15:36:35 +0800

    bottomMsg.

---
+++ PNPopBottomMsg/PNPopBottomMsg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PNPopBottomMsg
+//
+//  Created by lpn on 15/9/8.
+//  Copyright (c) 2015年 lpn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PNPopBottomMsg/PNPopBottomMsg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PNPopBottomMsg
+//
+//  Created by lpn on 15/9/8.
+//  Copyright (c) 2015年 lpn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
